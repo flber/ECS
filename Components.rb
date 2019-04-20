65faa319d6222a53ca12092e0ec238087e92ce34
flber <benrhammond@gmail.com>
2019-04-20 17:19:01 +1000

    Basic components

---
+++ Components.rb
@@ -0,0 +1,48 @@
+class Component
+  attr_reader :id
+
+  def initialize
+    @id = SecureRandom.uuid
+  end
+
+  def to_s
+    return "Component {#{id}: #{self.class.name}}"
+  end
+
+end
+
+#=================================================
+
+class Renderable < Component
+  attr_reader :image, :rotation
+
+  def initialize(file_name, rot)
+    @image = Gosu::Image.new(file_name)
+    @rotation = rot
+  end
+
+end
+
+#=================================================
+
+class Location < Component
+  attr_reader :x, :y, :dx, :dy
+
+  def initialize(x, y, dx, dy)
+    @x = x
+    @y = y
+    @dx = dx
+    @dy = dy
+  end
+
+end
+
+#=================================================
+
+class AffectedByGravity < Component
+end
+
+#=================================================
+
+class Movement < Component
+end
